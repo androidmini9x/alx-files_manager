7b5d292bdbda0e02184341e9afeabd4475f3b696
atck3r <atck3r@localdomain>
2023-12-05 19:31:24 +0300

    Fixed

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,16 +4,16 @@ import redisClient from '../utils/redis';
 class AppController {
   static getStatus(req, res) {
     const data = {
-      'redis': redisClient.isAlive(),
-      'db': dbClient.isAlive(),
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     };
     res.status(200).send(data);
   }
 
   static async getStats(req, res) {
     const data = {
-      'users': await dbClient.nbUsers(),
-      'files': await dbClient.nbFiles(),
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
     };
     res.status(200).send(data);
   }
